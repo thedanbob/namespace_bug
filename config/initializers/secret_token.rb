e433d24615370bb275312de8d471dfcff3aaf488
Daniel Arnfield <drarnfield@hwacollege.org>
2012-11-26 08:03:26 -0600

    Initial commit
    Signed-off-by: Daniel Arnfield <drarnfield@hwacollege.org>

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+NamespaceBug::Application.config.secret_key_base = '784e4aa2625d276e9407837ac66f27eab5617531e9d5aa74b54f126821b32541fe4936be55ffa689111a53562afe308a85d6b52c89d742c08873f40d56291674'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+NamespaceBug::Application.config.session_store :encrypted_cookie_store, key: '_namespace_bug_session'
